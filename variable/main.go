125ca0ce8f46dc50fc543de89cbd01b77b99bd40
Samuel <you@example.com>
2018-10-13 19:16:17 +0200

    variable

--- variable/main.go
+++ variable/main.go
@@ -4,8 +4,31 @@ import (
 	"fmt"
 )
 
+var y, x int
+var s = "hy"
+var bt = `Hy guys
+	how are you?`
+
+type ownType int
+
+var b ownType
+
 func main() {
-	n, err := fmt.Println("hy bitch")
+	y = 2
+	n, err := fmt.Println("hy guy")
 	fmt.Println(n)
 	fmt.Println(err)
+	x = 42
+	fmt.Println(x)
+	fmt.Printf("type: %T", x)
+	x = 12
+	fmt.Println(x + y)
+	fmt.Println(s)
+	b = 77
+	fmt.Println(b)
+	fmt.Printf("type: %T, %v\n", b, b+2)
+	bInt := int(b)
+	fmt.Println(bInt)
+	bs := string(b) // utf8
+	fmt.Println(bs)
 }
